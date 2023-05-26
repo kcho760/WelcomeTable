338a479ec51db24d631f03b4965c28706c9e2cb7
kcho760 <kcho760@gmail.com>
2023-05-26 16:46:34 -0400

    Added seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,39 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    validates :username, presence: true, length: {in: 3..30}, format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
-    validates :email, presence: true, length: {in: 3..255}, format: { with: URI::MailTo::EMAIL_REGEXP }
+    has_secure_password
+    before_validation :ensure_session_token
+
+    validates :username, presence: true, length: { within: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email" }
+    validates :email, presence: true, length: { within: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :session_token, presence: true, uniqueness: true
-    validates :password, length {in: 6..255}, allow_nil: true
+    validates :password, length: { within: 6..255 }, allow_nil: true
+    
+    def self.find_by_credentials(credential,password)
+        field = :username
+        user = User.find_by(field => credential)
+        user&.authenticate(password)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: generate_unique_session_token)
+        self.session_token
+    end
+
+
+    private
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom.base64
+            return session_token unless User.exists?(session_token: session_token)
+          end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+
 
 end
