1c53045fc60dbb5c795a91630429b6ff163128a9
kcho760 <kcho760@gmail.com>
2023-07-06 17:22:12 -0400

    Finished reservation login bug

--- frontend/src/components/RestaurantCarousel/restaurant_index_item.js
+++ frontend/src/components/RestaurantCarousel/restaurant_index_item.js
@@ -5,12 +5,14 @@ import RestaurantStarRating from "./subcomponents/restaurant_star_rating";
 import CostRating from "./subcomponents/cost_rating";
 import graph from "./assets/graph.png";
 import { Link, useHistory } from "react-router-dom";
+import LoginFormModal from '../LoginFormModal/index';
 
 function RestaurantIndexItem({ restaurant }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const [availableReservations, setAvailableReservations] = useState([]);
   const currentUser = useSelector((state) => state.session.user); // Access the current user from Redux state
+  const [showLoginFormModal, setShowLoginFormModal] = useState(false);
 
   useEffect(() => {
     const fetchReservations = async () => {
@@ -35,15 +37,19 @@ function RestaurantIndexItem({ restaurant }) {
     try {
       const modifiedReservationTime = new Date(reservation.reservationTime);
       modifiedReservationTime.setHours(modifiedReservationTime.getHours() + 1);
-      const currentUserId = currentUser.id; // Access the current user's ID from Redux state
       const partySize = 2; // Modify this as needed
-  
+      
+      if (!currentUser || !currentUser.id) {
+        setShowLoginFormModal(true);
+        return;
+      }
+      
       const reservationData = {
         reservation: {
           reservation_time: modifiedReservationTime.toISOString(),
           reservation_date: modifiedReservationTime.toISOString(),
           restaurant_id: restaurant.id,
-          user_id: currentUserId,
+          user_id: currentUser.id,
           party_size: partySize,
         },
       };
@@ -53,18 +59,18 @@ function RestaurantIndexItem({ restaurant }) {
       history.push(`/reservation/${reservationId}`);
     } catch (error) {
       console.error("Reservation Error:", error);
-      // Display an error message to the user or handle the error as needed
     }
   };
-  
-  
+    
   return (
     <div className="restaurant-item">
+      {showLoginFormModal && <LoginFormModal />}
       <div className="restaurant-image-container">
         <Link to={`/restaurants/${restaurant.id}`}>
           <img className="restaurant-image" src={restaurant.photoUrls[0]} alt={restaurant.name} />
         </Link>
       </div>
+
       <div className="restaurant-info">
         <Link to={`/restaurants/${restaurant.id}`}>
           <p className="restaurant-name">{restaurant.name}</p>
--- frontend/src/components/SearchIndexPage/SearchIndex.js
+++ frontend/src/components/SearchIndexPage/SearchIndex.js
@@ -1,27 +1,85 @@
-import React, { useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, useHistory } from 'react-router-dom';
 import './searchIndex.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSearchResults } from '../../store/search';
+import { fetchAvailableReservations, createReservation } from "../../store/reservation";
 import RestaurantStarRating from "../RestaurantCarousel/subcomponents/restaurant_star_rating";
 import SearchBar from '../Search_Bar';
 import CostRating from "../RestaurantCarousel/subcomponents/cost_rating";
+import LoginFormModal from '../LoginFormModal/index';
 
 function SearchIndex() {
   const { searchTerm = '' } = useParams();
   const dispatch = useDispatch();
+  const [availableReservations, setAvailableReservations] = useState([]);
+  const currentUser = useSelector((state) => state.session.user); // Access the current user from Redux state
+  const history = useHistory();
+  const [showLoginFormModal, setShowLoginFormModal] = useState(false);
 
-  useEffect(() => {
-      dispatch(getSearchResults(searchTerm));
-  }, [dispatch, searchTerm]);
 
   const searchResults = useSelector((state) => state.search.search.results) || [];
+
+  useEffect(() => {
+    dispatch(getSearchResults(searchTerm));
+  }, [dispatch, searchTerm]);
+
+  useEffect(() => {
+    const fetchReservations = async (restaurant) => {
+      const currentTime = new Date();
+      const startDateTime = new Date(Math.ceil(currentTime.getTime() / (15 * 60000)) * (15 * 60000));
+      const endDateTime = new Date(startDateTime.getTime() + 31 * 60000);
+      const reservationsParams = {
+        startDateTime: startDateTime.toISOString(),
+        endDateTime: endDateTime.toISOString(),
+        restaurantId: restaurant.id,
+        partySize: 2,
+      };
+    
+      const reservations = await dispatch(fetchAvailableReservations(reservationsParams));
+      setAvailableReservations(reservations?.availableReservations || []);
+    };  
+    if (searchResults.length > 0) {
+      fetchReservations(searchResults[0]); // Fetch reservations for the first restaurant in the search results
+    }
+  }, [searchResults]);
+  
+  
+  const handleReservationClick = async (reservation, restaurant) => {
+    try {
+      const modifiedReservationTime = new Date(reservation.reservationTime);
+      modifiedReservationTime.setHours(modifiedReservationTime.getHours() + 1);
+      const currentUserId = currentUser?.id; // Add the conditional check here
+      const partySize = 2;
+  
+      if (!currentUserId) {
+        setShowLoginFormModal(true);
+      }
+      const reservationData = {
+        reservation: {
+          reservation_time: modifiedReservationTime.toISOString(),
+          reservation_date: modifiedReservationTime.toISOString(),
+          restaurant_id: restaurant.id,
+          user_id: currentUserId,
+          party_size: partySize,
+        },
+      };
+
+      const createdReservation = await dispatch(createReservation(reservationData));
+      const reservationId = createdReservation.reservation.id;
+      history.push(`/reservation/${reservationId}`);
+    } catch (error) {
+      console.error('Reservation Error:', error);
+      // Display an error message to the user or handle the error as needed
+    }
+  };
 
   return (
     <div>
       <div className="search-index-search-container">
         <SearchBar className="search-index-search-bar" />
       </div>
+      {showLoginFormModal && <LoginFormModal />}
       <div className="search-result-container">
         <h1 className="search-result-text">
           Search Results for: {searchTerm ? searchTerm : 'All'}
@@ -30,18 +88,18 @@ function SearchIndex() {
       {Array.isArray(searchResults) ? (
         <ul>
           <div>
-          {searchResults.map((restaurant) => (
-            <div className="search-index-restaurant" key={restaurant.id}>
-              <div>
-                <Link to={`/restaurants/${restaurant.id}`}>
-                  <img className="search-image" src={restaurant.photoUrls[0]} alt="restaurant" />
-                </Link>
-                  <div className="restaurant-info">
-                <Link to={`/restaurants/${restaurant.id}`}>
-                    <h3>{restaurant.name}</h3>
+            {searchResults.map((restaurant) => (
+              <div className="search-index-restaurant" key={restaurant.id}>
+                <div>
+                  <Link to={`/restaurants/${restaurant.id}`}>
+                    <img className="search-image" src={restaurant.photoUrls[0]} alt="restaurant" />
                   </Link>
+                  <div className="restaurant-info">
+                    <Link to={`/restaurants/${restaurant.id}`}>
+                      <h3>{restaurant.name}</h3>
+                    </Link>
                     <div className="star-rating">
-                      <RestaurantStarRating restaurantId={restaurant.id}/>
+                      <RestaurantStarRating restaurantId={restaurant.id} />
                     </div>
                     <div className="restaurant-info-line-2">
                       <div>
@@ -51,14 +109,23 @@ function SearchIndex() {
                       <p className="restaurant-city">{restaurant.city}</p>
                     </div>
                     <div className="reservation-container">
-                      <button className="restaurant-reservation-button">10:00 AM</button>
-                      <button className="restaurant-reservation-button">10:15 AM</button>
-                      <button className="restaurant-reservation-button">10:30 AM</button>
+                      {availableReservations.map((reservation, index) => (
+                        <button
+                          key={index}
+                          className="restaurant-reservation-button"
+                          onClick={() => handleReservationClick(reservation, restaurant)}
+                        >
+                          {new Date(reservation.reservationTime).toLocaleTimeString([], {
+                            hour: '2-digit',
+                            minute: '2-digit',
+                          })}
+                        </button>
+                      ))}
                     </div>
                   </div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
           </div>
         </ul>
       ) : (
